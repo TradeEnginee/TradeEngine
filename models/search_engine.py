689b9193ade65b6275cf6cc128e13e5d14012fff
mona mohamed <mona.120230016@ejust.edu.eg>
2025-12-12 20:43:34 +0200

    Add polymorphism to product types and create search engine class

---
+++ models/search_engine.py
@@ -0,0 +1,12 @@
+class SearchEngine:
+    def __init__(self, name, version):
+        self.name = name
+        self.version = version
+
+    def search(self, query):
+        # Placeholder for search implementation
+        pass
+
+    def index(self, data):
+        # Placeholder for indexing implementation
+        pass    
